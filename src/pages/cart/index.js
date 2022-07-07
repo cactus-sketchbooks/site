b38011f284689925c81b92b34496d287ba0f3db3
Higor Gonçalves Brandão <higorb2000@gmail.com>
2022-07-07 10:55:51 -0300

    teste

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -623,7 +623,6 @@ export default function Cart() {
                 firebase.database().ref('requests/' + id).set(dataToSend)
 
                 firebase.database().ref('reportsSales/' + id).set(dataToSend)
-                setTimeout(() => { localStorage.setItem('products', '{}') }, 5000);
 
             } else {
 
@@ -664,7 +663,6 @@ export default function Cart() {
                 firebase.database().ref('requests/' + id).set(dataToSend)
 
                 firebase.database().ref('reportsSales/' + id).set(dataToSend)
-                setTimeout(() => { localStorage.setItem('products', '{}') }, 5000);
 
             }
 
